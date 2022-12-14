796e35f7f981142c5f140995e4379501701731cf
Dante de Lordran <dantelopez305@outlook.com>
2022-12-13 20:47:56 -0600

    Home screen with stateless widget finished

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -3,10 +3,47 @@ import 'package:flutter/material.dart';
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
-  @override
+  @override //El buildcontext es el arbol de widgets
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Buenas noches Flutter'),
+    TextStyle fontSize = const TextStyle(fontSize: 25);
+
+    //El scaffold es la ventana en la que viene el contenido
+    return Scaffold(
+      backgroundColor: Colors.yellow,
+      appBar: AppBar(
+        backgroundColor: Colors.pink,
+        elevation: 4, //Quita el sombreado que pone debajo del widget
+        title: Text(
+          'Titulo del appbar',
+          style: fontSize,
+        ),
+      ),
+      body: Center(
+        child: Column(
+          //Alinea en Y
+          mainAxisAlignment: MainAxisAlignment.center,
+          //Alinea en X y por defecto esta centrado
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Deliver features faster',
+              style: fontSize,
+            ),
+            Text(
+              'Craft beautiful UIs',
+              style: fontSize,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.startFloat,
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.pink,
+        child: const Icon(
+          Icons.add,
+        ),
+        onPressed: () {},
+      ),
     );
   }
 }
